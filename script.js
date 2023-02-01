9ee235d955253f45ead749bf7688c2310533fea3
greyseth <anargya2gilland@gmail.com>
2023-02-01 11:26:04 +0700

    Fixed movement

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let vector2 = { x: 0, y: 0 };
 const speed = 15;
 
 const player = document.getElementById("player");
+let gameRunning = true;
+
+let pressA = false;
+let pressD = false;
 
 document.addEventListener(
   "keydown",
@@ -10,36 +14,47 @@ document.addEventListener(
       player.classList.remove("idle");
       player.classList.add("run");
       player.classList.remove("mirrored");
-      vector2.x -= speed;
-      moveLeft();
+      pressA = true;
     } else if (e.key === "d") {
       player.classList.remove("idle");
       player.classList.add("run");
       player.classList.add("mirrored");
-      vector2.x += speed;
-      moveRight();
+      pressD = true;
     }
-
-    // updatePosition();
   },
   false
 );
 
 document.addEventListener("keyup", function (e) {
-  if (e.key === "a" || e.key === "d") {
+  if (e.key === "a") {
     player.classList.add("idle");
     player.classList.remove("run");
+    pressA = false;
+  } else if (e.key === "d") {
+    player.classList.add("idle");
+    player.classList.remove("run");
+    pressD = false;
   }
 });
 
 function updatePosition() {
-  player.style.transform = `translateX(${Math.round(vector2.x)}px);`;
+  player.style.left = `${vector2.x}px`;
 }
 
-function moveLeft() {
-  player.style.left = parseInt(vector2.x) - speed + "px";
-}
+function playerGravity() {}
 
-function moveRight() {
-  player.style.left = parseInt(vector2.x) + speed + "px";
-}
+window.onload = function () {
+  function update() {
+    if (pressA) {
+      vector2.x -= speed;
+    }
+
+    if (pressD) {
+      vector2.x += speed;
+    }
+
+    updatePosition();
+  }
+
+  setInterval(update, 100);
+};
